a65be6868d84fef918ee224aeab224b249f8789f
Ubuntu <ubuntu@ip-172-31-45-167.us-east-2.compute.internal>
2020-03-05 08:47:49 +0000

    -

--- src/static/hbsHelpers.js
+++ src/static/hbsHelpers.js
@@ -9,15 +9,20 @@ Handlebars.registerHelper('times', function(total, active, block) {
 
 Handlebars.registerHelper('stars-remind', function(target, missioners, block) {
     var accum = 0;
-    missioners.forEach(el => accum += el.stars)
+    for(let i = 0; i < missioners.length; i++){
+        accum += missioners[i].stars
+    }
+    // missioners.forEach(el => accum += el.stars)
     return target - accum;  
 });
 
 Handlebars.registerHelper('finished', function(target, missioners, block) {
     var accum = 0;
-    missioners.forEach(el => accum += el.stars)
+    for(let i = 0; i < missioners.length; i++){
+        accum += missioners[i].stars
+    }
+    // missioners.forEach(el => accum += el.stars)
     const remindStars =  target - accum;
     if (remindStars) return ''
     else return 'Conseguido!' 
 });
-
