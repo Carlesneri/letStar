181b38f44920d8a572361997c5572807f6232974
Carlesneri <carlesneri@hotmail.com>
2020-03-23 09:18:26 +0100

    Ajuste add remove star y navs

--- src/static/mission.js
+++ src/static/mission.js
@@ -44,49 +44,61 @@ async function active(e){
     }
 
     async function addStar(id, missioner){
-        const data = {id, missioner}
-        target.classList.add("active-star")
-
-        await fetch("/mission/add-star", {
-            method: "PUT",
-            body: JSON.stringify(data),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-
-        const res = await fetch(`/mission/${id}`)       
-        const response = await res.json()        
-        const {mission} = response
-        const totalStars = getTotalStars(mission)   
-        document.querySelector($remindStars).innerText = mission.target - totalStars
+        try{
+            const data = {id, missioner}
+            await fetch("/mission/add-star", {
+                method: "PUT",
+                body: JSON.stringify(data),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            
+            const res = await fetch(`/mission/${id}`)       
+            const response = await res.json()        
+            const {mission} = response
+            target.classList.add("active-star")
+            const totalStars = getTotalStars(mission)   
+            document.querySelector($remindStars).innerText = mission.target - totalStars
+        }catch(err){
+            console.log('Error al añadir estrella. Error: ', err);
+            Swal.fire({
+                html: "Error al añadir estrella",
+                showConfirmButton: false,
+                timer: 1700,
+            })
+        }
     }
     
     async function removeStar(id, missioner){
-        const data = {id, missioner}
-
-	target.classList.remove("active-star")        
-
-        target.classList.remove("active-star")
-
-        await fetch("/mission/remove-star", {
-            method: "PUT",
-            body: JSON.stringify(data),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-
-        const res = await fetch(`/mission/${id}`)
-        
-        const response = await res.json()
-        
-        const {mission} = response
-        
-        const totalStars = getTotalStars(mission)        
-        document.querySelector($remindStars).innerText = mission.target - totalStars
+        try{
+            const data = {id, missioner}
+    
+            await fetch("/mission/remove-star", {
+                method: "PUT",
+                body: JSON.stringify(data),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            
+            const res = await fetch(`/mission/${id}`)
+            const response = await res.json()
+            const {mission} = response
+            target.classList.remove("active-star")        
+            const totalStars = getTotalStars(mission)        
+            document.querySelector($remindStars).innerText = mission.target - totalStars
+        }catch(err){
+            console.log('Error al quitar estrella. Error: ', err);
+            Swal.fire({
+                html: "Error al quitar estrella",
+                showConfirmButton: false,
+                timer: 1700,
+            })
+        }
     }
 }
+
 
 function getTotalStars(mission){
     var accum = 0;
