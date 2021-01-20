ae28d2010459d4dabce5494ca2a1cb4f7c08d944
Carlesneri <carlesneri@hotmail.com>
2021-01-20 11:39:05 +0100

    trying nginx

--- src/index.js
+++ src/index.js
@@ -11,23 +11,25 @@ const privateKey = fs.readFileSync(path.join(__dirname, '../SSL/private.key'), '
 const certificate = fs.readFileSync(path.join(__dirname, '../SSL/certificate.crt'), 'utf8')
 const caBundle = fs.readFileSync(path.join(__dirname, '../SSL/ca_bundle.crt'), 'utf8')
 
-if(process.env.DEVELOPMENT === 'true'){
-    http.createServer(app)
-    .listen(3080, () => console.log('Server listening on port ', 3080))
-}else{
-    https.createServer({
-        key: privateKey,
-        cert: certificate,
-        ca: caBundle
-    }, app)
-        .listen(3443, () => console.log('Server listening on port 3443'))
+// if(process.env.DEVELOPMENT === 'true'){
+//     http.createServer(app)
+//     .listen(3080, () => console.log('Server listening on port ', 3080))
+// }else{
+//     https.createServer({
+//         key: privateKey,
+//         cert: certificate,
+//         ca: caBundle
+//     }, app)
+//         .listen(3443, () => console.log('Server listening on port 3443'))
 
-        console.log('Redirecting https');
+//         console.log('Redirecting https');
     
-    http.createServer((req, res) => {
-            res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
-            res.end();
-        })
-        .listen(3080, () => console.log('Server listening on port ', 3080))
-}
+//     http.createServer((req, res) => {
+//             res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+//             res.end();
+//         })
+//         .listen(3080, () => console.log('Server listening on port ', 3080))
+// }
 
+http.createServer(app)
+.listen(3000, () => console.log('Server listening on port ', 3000))
