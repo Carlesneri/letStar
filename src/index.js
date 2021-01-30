4955b68666bc190e85501fed070169248d2ce478
Carlesneri <carlesneri@hotmail.com>
2021-01-30 09:34:05 +0100

    hbs version

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 const app = require('./server')
 const http = require ('http')
-const https = require ('https')
-const fs = require('fs')
-const path = require('path')
-const dotenv = require('dotenv')
+// const https = require ('https')
+// const fs = require('fs')
+// const path = require('path')
+// const dotenv = require('dotenv')
 
-dotenv.config()
+// dotenv.config()
 
-const privateKey = fs.readFileSync(path.join(__dirname, '../SSL/private.key'), 'utf8')
-const certificate = fs.readFileSync(path.join(__dirname, '../SSL/certificate.crt'), 'utf8')
-const caBundle = fs.readFileSync(path.join(__dirname, '../SSL/ca_bundle.crt'), 'utf8')
+// const privateKey = fs.readFileSync(path.join(__dirname, '../SSL/private.key'), 'utf8')
+// const certificate = fs.readFileSync(path.join(__dirname, '../SSL/certificate.crt'), 'utf8')
+// const caBundle = fs.readFileSync(path.join(__dirname, '../SSL/ca_bundle.crt'), 'utf8')
 
 // if(process.env.DEVELOPMENT === 'true'){
 //     http.createServer(app)
@@ -32,4 +32,4 @@ const caBundle = fs.readFileSync(path.join(__dirname, '../SSL/ca_bundle.crt'), '
 // }
 
 http.createServer(app)
-.listen(3000, () => console.log('Server listening on port ', 3000))
+.listen(app.get('port'), () => console.log('Server listening on port ', app.get('port')))
--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ const app = express();
 require('./auth/local')
 
 //-->Settings
-app.set('PORT', process.env.PORT || 80);
+app.set('port', 3001);
 app.set('views', path.join(__dirname, 'views'));
 app.set('static', path.join(__dirname, 'static'));
 app.engine('.hbs', exphbs({
